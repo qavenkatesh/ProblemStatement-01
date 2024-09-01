2c2d71352706130697885b83baea9d4f4823c662
svenk <svenk@VenkyPC>
2024-09-01 11:51:14 +0530

    Initial commit

---
+++ src/test/java/railYatri/BookTicket.java
@@ -0,0 +1,26 @@
+package railYatri;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BookTicket {
+
+	@Test
+	public void LaunchUrl() {
+		// TODO Auto-generated method stub	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\svenk\\eclipse-workspace\\AutomePage\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.railyatri.in/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.close();
+	}
+	
+	@Test (dependsOnMethods = {"LaunchUrl"})
+	public void BookAny() {
+		System.out.println("SecondTestcase");
+	}
+
+	
+}
