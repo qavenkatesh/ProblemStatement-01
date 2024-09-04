f21329f99f98c7afbe11a4e1308efae875a68c55
svenk <svenk@VenkyPC>
2024-09-04 19:18:48 +0530

    Assignment 01 completed

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,46 +4,33 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import utilities.CommonUtils;
 
 
 public class BaseTest extends CommonUtils {
-	
+
 	public static Properties prop = new Properties();;
 	public static FileReader fr;
 	
+
 	@BeforeTest
-	public void setup() throws IOException {
-		if(driver==null) {
-			fr = new FileReader(System.getProperty("user.dir")+"\\configfile\\config.properties");
+	public static void setup() throws IOException {
+
+			fr = new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\Config file\\config.properties");
 			prop.load(fr);
-		}
-		
-		if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\chromedriver\\chromedriver.exe");
-			driver = new ChromeDriver();
+			options = new ChromeOptions();
+			driver = new ChromeDriver(options);
 			driver.get(prop.getProperty("url"));
-			maximize();
-			stay();
-			System.out.println("Page launched");
-		}
-		if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.firefox.driver", "C:\\Users\\svenk\\eclipse-workspace\\AutomePage\\firefoxdriver\\firefoxdriver.exe");
-			driver = new ChromeDriver();
-			driver.get(prop.getProperty("url"));
-			stay();
-			maximize();
-			System.out.println("Page launched");
-			
-		}			
+			maximize();		
 	}
 	
 	@AfterTest
-	public void teardown() {
+	public static void teardown() {
 		driver.close();
-		System.out.println("Browser closed");
 	}
 
 }
--- src/test/java/utilities/CommonUtils.java
+++ src/test/java/utilities/CommonUtils.java
@@ -1,26 +1,36 @@
 package utilities;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonUtils {
 	public static WebDriver driver;
+	public static ChromeOptions options;
 	public static WebDriverWait wait;
 
-	@SuppressWarnings("deprecation")
-	public static void stay(){
-		//wait = new WebDriverWait(driver,Duration.ofSeconds(50));
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-	}
 	
 	public static void maximize() {
 		driver.manage().window().maximize();
 	}
 	
-	public void ReadData(String excelsheetname) {
-
+	public static void readExcel() throws IOException {
+		FileInputStream fis = new FileInputStream("C:\\Users\\svenk\\eclipse-workspace\\AutomePage\\src\\test\\resources\\Test Data\\testdata.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet1 = wb.getSheetAt(0);
+		for (Row row : sheet1) {
+			for(org.apache.poi.ss.usermodel.Cell cell : row) {
+				System.out.println(cell.toString()+"\t");
+			}
+		}
+		wb.close();
+		fis.close();
+		
 	}
 
 }
