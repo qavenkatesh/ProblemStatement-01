a391f648fdff68bd4045073280fa2b315e8c7ef3
svenk <svenk@VenkyPC>
2024-09-06 14:53:01 +0530

    Excel read data

---
+++ src/test/java/testcases/TestCase01.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+public class TestCase01 extends BaseTest {
+	
+	@Test
+	public static void fullname() throws IOException {
+		
+		readExcel();
+		
+	}
+
+}
